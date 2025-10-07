5bc97952d2cfe887f1c17c1fa1b1018e61f5d00e
Lianagodoy <lianagodoy@terra.com.br>
2025-10-07 18:23:14 -0400

    fix: corrigir configuração do supabaseClient

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,6 @@
-// lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(url, anonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
